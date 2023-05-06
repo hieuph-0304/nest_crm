8a0ad99162b97ecf72faa237f96334976593a802
hieuph-0968 <phan.hoang.hieu@sun-asterisk.com>
2023-05-06 10:22:00 +0700

    Add module health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, HealthModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExceptionLoggerFilter } from './utils/exception-logger-filter';
+import { AppExceptionFilter, ExceptionInterceptor } from 'nestjs-error-handler';
+import { Logger } from '@nestjs/common';
+import { HTTP_STATUS, TIME_ZONE } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new ExceptionLoggerFilter(httpAdapter));
+  app.useGlobalFilters(new AppExceptionFilter(new Logger(), HTTP_STATUS, TIME_ZONE.ASIA_TOKYO));
+  app.useGlobalInterceptors(new ExceptionInterceptor());
 
   await app.listen(3000);
 }
